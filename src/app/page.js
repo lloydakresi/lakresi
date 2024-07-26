e77e00d03748442f5adc2b9bef00478378548825
lloydakresi <lloydakresi03@gmail.com>
2024-07-26 06:27:16 +0000

    1.0.10

--- src/app/page.js
+++ src/app/page.js
@@ -55,7 +55,7 @@ export default function Home() {
   </section>
 </div>
     </main>
-    <footer className="flex flex-col justify-center items-center py-5 font-Ubuntu text-xl">
+    <footer className="flex flex-col justify-center items-center text-center py-5 font-Ubuntu text-sm">
       Made with ❤️ in Ghana <br/>
       &copy; 2024 Lloyd Akresi
     </footer>
